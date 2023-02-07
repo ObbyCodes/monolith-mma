607a3de0b4c0fd05901d27169d34fc997ddd1017
ObbyCodes <obbycodes@gmail.com>
2023-02-07 21:51:10 +0000

    First commit

---
+++ src/main/java/com/obby/monolith/monolith/mma/MonolithMmaApplication.java
@@ -0,0 +1,13 @@
+package com.obby.monolith.monolith.mma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithMmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolithMmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/obby/monolith/monolith/mma/MonolithMmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obby.monolith.monolith.mma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithMmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
